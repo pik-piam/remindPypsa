61d65ff6bd7de01266cffebe3063ae2443693c27
Adrian Odenweller <adrian.odenweller@pik-potsdam.de>
2023-03-25 14:09:12 +0100

    Update StartPyPSA.sh

--- inst/extdata/StartPyPSA.sh
+++ inst/extdata/StartPyPSA.sh
@@ -3,5 +3,5 @@
 # Pass iteration number and name of output directory as command line argument
 echo "Starting PyPSA in iteration $1, output folder $2"
 cd "$(dirname "$0")"
-source venv/bin/activate
-snakemake -s Snakefile_REMIND_solve --profile cluster_config/ export_all_networks_rm --config iter=$1 RDIR_rm=$2
+source /home/adrianod/software/venv_pypsa-eur/bin/activate
+snakemake -call -s Snakefile_remind --profile cluster_config/ export_all_networks_remind --config iter=$1 RDIR_remind=$2 opt=solve
